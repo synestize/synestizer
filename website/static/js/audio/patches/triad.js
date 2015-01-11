8a1fc11393499d5c86f235e7602d7659006c3063
howthebodyworks <git@email.possumpalace.org>
2015-01-11 19:00:35 +0100

    switch triad synth to pubsub system

--- website/static/js/audio/patches/triad.js
+++ website/static/js/audio/patches/triad.js
@@ -1,20 +1,15 @@
+
+pubsub.subscribe(
+    "/videoanalyzer/averagecolor/raw",
+    function(data) {
+        triad.glide1( document.getElementById('triadFreq1Knob').value + ( 0.05 * Math.pow(~~(data[0]*255), 2)) );
+        triad.glide2( document.getElementById('triadFreq2Knob').value + ( 0.05 * Math.pow(~~(data[1]*255), 2)) );
+        triad.glide3( document.getElementById('triadFreq3Knob').value + ( 0.05 * Math.pow(~~(data[2]*255), 2)) );
+    });
+
+
 triad = WX.Triad();
 triad.to(WX.Master);
 
-
-/*
-MUI.start(function () {
-
-    triad = WX.Triad();
-    triad.to(WX.Master);
-
-    triad.to(MUI.$('spec'));
-    MUI.buildControls(triad, 'rack');
-
-//triad.noteOn1(63, 50);
-//triad.noteOn2(75, 50);
-
-});
-*/
 
 
