c067eb87e61ba494f777b7716fb334ab921d0964
howthebodyworks <git@email.possumpalace.org>
2015-04-24 20:40:59 +0200

    initial preset

---
+++ offline/presets/patch1.js
@@ -0,0 +1,26 @@
+{
+  "triad":{
+    "oscFreq1":440,
+    "oscFreq3":440,
+    "oscFreq2":440,
+    "oscType3":"sine",
+    "oscType2":"sine",
+    "oscType1":"sine",
+    "output":0.3
+  },
+  "minimal":{
+    "ampAttack":0.0,
+    "tempo":200,
+    "ampDecay":0.2,
+    "ampRelease":0.5,
+    "output":0.0,
+    "ampSustain":1.0
+  },
+  "sampler":{
+    "ampAttack":0.0,
+    "output":0.0,
+    "ampSustain":0.25,
+    "ampDecay":0.2,
+    "ampRelease":0.0
+  }
+}
