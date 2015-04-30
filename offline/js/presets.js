3f5f3360c704329ee1ec63f13610349ce5aed718
howthebodyworks <git@email.possumpalace.org>
2015-04-30 19:17:18 +0200

    retreive a real preset by hacking syntestizer.com

--- offline/js/presets.js
+++ offline/js/presets.js
@@ -1,30 +1,53 @@
 var app = app || {};
 
-app.presets = {
-  default: {
-  "triad":{
-    "oscFreq1":440,
-    "oscFreq3":440,
-    "oscFreq2":440,
-    "oscType3":"sine",
-    "oscType2":"sine",
-    "oscType1":"sine",
-    "output":0.3
-  },
-  "minimal":{
-    "ampAttack":0.0,
-    "tempo":200,
-    "ampDecay":0.2,
-    "ampRelease":0.5,
-    "output":0.0,
-    "ampSustain":1.0
-  },
-  "sampler":{
-    "ampAttack":0.0,
-    "output":0.0,
-    "ampSustain":0.25,
-    "ampDecay":0.2,
-    "ampRelease":0.0
-  }
-  }
-};
+app.defaultModel = {
+    "color_mode": {
+        "created": "2014-11-27T17:02:59.128543",
+        "id": 1,
+        "modified": "2014-11-27T17:02:59.128577",
+        "name": "RGB",
+        "preset": "",
+        "resource_uri": "/api/v1/synth/color_mode/1",
+        "slug": "rgb",
+        "uuid": "df0c18b7f76a4e2586ebbdb45d8681f5"
+    },
+    "created": "2014-11-27T17:03:55.568368",
+    "id": 1,
+    "modified": "2015-01-19T16:13:19.241946",
+    "preset": "{\"triad\": {\"oscFreq1\": 440, \"oscFreq3\": 440, \"oscFreq2\": 440, \"oscType3\": \"sine\", \"oscType2\": \"sine\", \"oscType1\": \"sine\", \"output\": 0.3}, \"minimal\": {\"ampAttack\": 0.0, \"tempo\": 200, \"ampDecay\": 0.2, \"ampRelease\": 0.5, \"output\": 0.0, \"ampSustain\": 1.0}, \"sampler\": {\"ampAttack\": 0.0, \"output\": 0.0, \"ampSustain\": 0.25, \"ampDecay\": 0.2, \"ampRelease\": 0.0}}",
+    "publish": "2014-11-27T17:03:49",
+    "resource_uri": "/api/v1/synth/patch/1",
+    "synth": [
+        {
+            "created": "2014-11-27T17:03:35.452647",
+            "id": 2,
+            "modified": "2015-01-06T15:36:43.058858",
+            "name": "Minimal",
+            "preset": "null",
+            "resource_uri": "/api/v1/synth/synth/2",
+            "slug": "minimal",
+            "uuid": "7887d126f28b48e0b2a84a366d798c19"
+        },
+        {
+            "created": "2015-01-16T17:57:41.714003",
+            "id": 3,
+            "modified": "2015-01-16T17:57:41.714052",
+            "name": "Sampler",
+            "preset": "null",
+            "resource_uri": "/api/v1/synth/synth/3",
+            "slug": "sampler",
+            "uuid": "3396275084fc4d34b0e4a80acde9949a"
+        },
+        {
+            "created": "2014-11-27T17:02:25.680258",
+            "id": 1,
+            "modified": "2015-01-18T23:53:48.224620",
+            "name": "Triad",
+            "preset": "{\"oscFreq1\": 500, \"oscFreq3\": 880, \"oscFreq2\": 440, \"oscType3\": \"square\", \"oscType2\": \"triangle\", \"oscType1\": \"sine\", \"oscFreq3Max\": 1000, \"oscFreq2Max\": 1000, \"oscFreq3Min\": 0, \"oscFreq1Min\": 0, \"output\": 0.1, \"oscFreq1Max\": 1000, \"oscFreq2Min\": 0}",
+            "resource_uri": "/api/v1/synth/synth/1",
+            "slug": "triad",
+            "uuid": "be6f465b40e7472c9fbee556e32d5899"
+        }
+    ],
+    "uuid": "7f75fe647ce34e00b6c5c9c3a03cd533"
+}
