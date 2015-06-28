d8707b22746fe59a5c89c97a5b2c0ed4286d3ab3
howthebodyworks <git@email.possumpalace.org>
2015-06-28 19:34:56 +0200

    sketch synth structure

--- offline/js/audio/instruments/basic.js
+++ offline/js/audio/instruments/basic.js
@@ -1,8 +1,13 @@
 (function(window){
     'use strict';
-    var self=this;
-
-
-    window.Recorder = Recorder;
-
+    var Basic;
+    window.Instruments = window.Instruments || {};
+    Basic = function (parms) {
+        
+    }
+    Basic.prototype.controls = {
+        gain: {scale: d3.scale.linear(), units: "db"}
+    }
+    window.Instruments.Basic = Basic;
+    
 })(window);
