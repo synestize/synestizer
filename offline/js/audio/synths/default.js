0f3ad53869807e5c1d59850917999b47f44a1869
howthebodyworks <git@email.possumpalace.org>
2015-06-30 22:10:23 +0200

    hmmm maybe i DO need WAAX

--- offline/js/audio/synths/default.js
+++ offline/js/audio/synths/default.js
@@ -6,18 +6,23 @@ State models reflect the current state of the model
     'use strict';
     var synths;
     window.synths = synths = window.synths || {};
-    synths.defaultOpts = {
+    synths.defaultOptions = {
         sustain: 1,
         pitch: 60,
+        tempo: 1,
     }
-    synths.Default = function(ctx, when, options) {
+    synths.Default = function(ctx, startTime, options) {
         var oscnode, envnode, out;
         options = _.extend(synths.defaultOptions, options);
         out = options.out || ctx.destination;
+        envnode = cts.createGain();
+        envnode.gain.value = 0;
         oscnode = context.createOscillator();
+        oscnode.connect(envnode);
         oscnode.type = vco.SINE;
         oscnode.frequency.value = this.frequency;
-        oscnode.start(0);
+        oscnode.start(startTime || 0);
+        return
     }
 
 })(window, _);
