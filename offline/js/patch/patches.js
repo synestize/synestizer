5bc8ed6320435cf9cf4f94dc76cd89808a12370e
howthebodyworks <git@email.possumpalace.org>
2015-07-06 19:22:11 +0200

    move patches out to the top of the hierarchy; they are important.

---
+++ offline/js/patch/patches.js
@@ -0,0 +1,36 @@
+// Patches
+// wires up staistics and synthesis using code
+// We would ideally like to be able to *save* patches
+(function( global, _, Rx ) {
+    'use strict';
+    var patch; 
+    window.patch = patch = window.patch || {};
+
+    function basic_triad(videoPixels, controlSidebar) {
+        //simple example patch
+        var triadEnsembleParamSet;
+        var triadEnsembleView;
+        var triadEnsemble;
+        var statsStreamer;
+        var self = {
+        };
+        
+        statsStreamer = videoanalysis.StatsStreamer(videoPixels, {
+            avg: videoanalysis.AverageColor(),
+            //cov: videoanalysis.PluginMoments(),
+        });
+        
+        triadEnsemble = ensembles.TriadEnsemble("triad1");
+        triadEnsembleParamSet = ensembles.EnsembleParamSet(
+            triadEnsemble);
+        triadEnsembleView = ensembles.EnsembleView(
+            triadEnsembleParamSet,
+            controlSidebar
+        );
+        statsStreamer.statsStream.pluck("avg", 0).subscribe(function(x){console.debug(x)})
+        return self;
+    };
+
+    // expose our module to the global object
+    global.patch.basic_triad = basic_triad;
+})( this, _, Rx );
