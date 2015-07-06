1564d0c820d96c3109b724555ccdb382a44e0df8
howthebodyworks <git@email.possumpalace.org>
2015-07-06 16:45:33 +0200

    match statistics to new pixelpump api

--- offline/js/video/_video.js
+++ offline/js/video/_video.js
@@ -1,27 +1,48 @@
 // Video Analyzer
 // pumps pixels in to statistics functions, pumps them out later
+//TODO: reinstate delta stats
 (function( global, _, Rx ) {
     'use strict';
     var video; 
     window.video = video = window.video || {};
 
-    function StatsStreamer(pixelStreamer, stats){
+    function StatsStreamer(pixelPump, stats){
         // should probably make this stats array be serializable if I want to 
-        // do it in another thread.
+        // do it in a webworker.
         var lastTime, thisTime;
-        var statsStream;
+        var inboxStream; //incoming pixels
+        var outboxStream;//outgoing analyses
         var statsWorkers;
         var statsState = {};
-        
-        statsStream = new Rx.BehaviorSubject(1);
-        
+        console.debug("ss", pixelPump, stats);
+        inboxStream = new Rx.Subject();
+        //this bit should be in a WebWorker:
+        outboxStream = inboxStream.map(function (pixels) {
+            var rez = _.mapObject(
+                stats,
+                function(stat, statName){return stat(pixels)}
+            );
+            console.debug("statres", pixels, rez);
+            return rez;
+        });
+        outboxStream.debounce(25).subscribe(function() {
+            pixelPump.pump(function(pixels){inboxStream.onNext(pixels)});
+        });
+        //update state dict with stats for debugging
+        outboxStream.subscribe(function(x){
+            _.mapObject(
+                x,
+                function(statval, statName){statsState[statval]=statName}
+            );
+        });
         var self = {
-            statsStream: statsStream,
+            statsStream: outboxStream,
             statsState: statsState
         };
         lastTime = Date.now();
         thisTime = Date.now();
-
+        //prime pixel pump
+        pixelPump.pump(function(pixels){inboxStream.onNext(pixels)});
         return self;
     };
 
