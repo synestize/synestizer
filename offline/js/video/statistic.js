d7b7231756e363126c3506095ed3cd6ba7b3657e
howthebodyworks <git@email.possumpalace.org>
2015-07-06 16:45:13 +0200

    neaten up statistics for new pixelpump

--- offline/js/video/statistic.js
+++ offline/js/video/statistic.js
@@ -9,27 +9,25 @@
     var video; 
     window.video = video = window.video || {};
     function AverageColor(params) {
-        var cw, ch, pixelCount;
+        var PIXELDIM=64; //64x64 grid is all we use.
+        var PIXELCOUNT=PIXELDIM*PIXELDIM;
         params = params || {};
         var out = new Float32Array(3);
         var R=0, G=1, B=2;
-        cw = params.cw || 86;
-        ch = params.ch || 64;
-        pixelCount = cw * ch;
         function calc(pixels) {
             out[R] = 0;
             out[G] = 0;
             out[B] = 0;
             
-            for (var i = 0; i < pixelCount; i++) {
+            for (var i = 0; i < PIXELCOUNT; i++) {
                 out[R] += pixels[i * 4 + R]/255;
                 out[G] += pixels[i * 4 + G]/255;
                 out[B] += pixels[i * 4 + B]/255;
             }
 
-            out[R] = out[R] / pixelCount;
-            out[G] = out[G] / pixelCount;
-            out[B] = out[B] / pixelCount;
+            out[R] = out[R] / PIXELCOUNT;
+            out[G] = out[G] / PIXELCOUNT;
+            out[B] = out[B] / PIXELCOUNT;
             return out;
         }
         calc.nDims = 3;
@@ -38,87 +36,91 @@
     // expose our module to the global object
     global.video.AverageColor = AverageColor;
     
-    function Covariance(params) {
-        var cw, ch, pixelCount;
-        var rawSums, centralMoments, cookedMoments, bsvxy;
+    function PluginMoments(params) {
+        // Gives us moment estimates by the plugin method
+        // right now, 1st and 2nd central moments
+        // a.k.a. mean and covariance
+        var PIXELDIM=64; //64x64 grid is all we use.
+        var PIXELCOUNT=PIXELDIM*PIXELDIM;
+        var rawSums, centralMoments, cookedMoments, ysvij;
         
-        // I call the YCBCR mapped version "BSV", the spatial coords "XY"
-        var B=0, S=1, V=2, XB=4, XS=5, XV=6, YB=7, YS=8, YV=9, BB=10, BS=11, BV=12, SS=13, SV=14, VV=15;
+        // I call the YCbCr mapped version "YSV", the spatial coords "IJ"
+        var B=0, S=1, V=2, IB=4, IS=5, IV=6, JB=7, JS=8, JV=9, BB=10, BS=11, BV=12, SS=13, SV=14, VV=15;
         params = params || {};
         
         rawSums = new Float32Array(16);
         centralMoments = new Float32Array(16);
         cookedMoments = new Float32Array(16);
-        bsvxy = new Float32Array(3);
-        cw = params.cw || 86;
-        ch = params.ch || 64;
-        pixelCount = cw * ch;
+        ysvij = new Float32Array(3);
         
         function calc(pixels) {
-            for (var i = 0; i < ch; i++) {
-                for (var j = 0; i < cw; j++) {
-                    var ij = (cw * i + j) * 4;
-                    bsvxy[0] = ( //B
+            for (var i = 0; i < PIXELDIM; i++) {
+                for (var j = 0; i < PIXELDIM; j++) {
+                    var ij = (PIXELDIM * i + j) * 4;
+                    // first we tranform RGB to YSV 
+                    // - effectively a PCA of the color vectors
+                    // otherwise we are measuring mostly brightness.
+                    ysvij[0] = ( //Y
                           0.00117255*pixels[ij]
                         + 0.00230200*pixels[ij+1]
                         + 0.00044706*pixels[ij+2]
                     );
-                    bsvxy[1] = 0.5 + ( //S
+                    ysvij[1] = 0.5 + ( //S
                         - 0.00066563*pixels[ij] 
                         - 0.00129907*pixels[ij+1]
                         + 0.00196078*pixels[ij+2]
                     );
-                    bsvxy[2] = 0.5 + ( //V
+                    ysvij[2] = 0.5 + ( //V
                           0.00196078*pixels[ij]
                         - 0.00164191*pixels[ij+1]
                         - 0.00031887*pixels[ij+2]
                     );
-                    bsvxy[3] = j/ch; //X
-                    bsvxy[4] = i/cw; //Y
-                    rawSums[B] += bsvxy[0];
-                    rawSums[S] += bsvxy[1];
-                    rawSums[V] += bsvxy[2];
-                    rawSums[XB] += bsvxy[0]*bsvxy[3];
-                    rawSums[XS] += bsvxy[1]*bsvxy[3];
-                    rawSums[XV] += bsvxy[2]*bsvxy[3];
-                    rawSums[YB] += bsvxy[0]*bsvxy[4];
-                    rawSums[YS] += bsvxy[1]*bsvxy[4];
-                    rawSums[YV] += bsvxy[2]*bsvxy[4];
-                    rawSums[BB] += bsvxy[0]*bsvxy[0];
-                    rawSums[BS] += bsvxy[0]*bsvxy[1];
-                    rawSums[BV] += bsvxy[0]*bsvxy[2];
-                    rawSums[SS] += bsvxy[1]*bsvxy[1];
-                    rawSums[SV] += bsvxy[0]*bsvxy[2];
-                    rawSums[VV] += bsvxy[2]*bsvxy[2];
+                    ysvij[3] = j/PIXELDIM; //I
+                    ysvij[4] = i/PIXELDIM; //J
+                    rawSums[B] += ysvij[0];
+                    rawSums[S] += ysvij[1];
+                    rawSums[V] += ysvij[2];
+                    rawSums[IB] += ysvij[0]*ysvij[3];
+                    rawSums[IS] += ysvij[1]*ysvij[3];
+                    rawSums[IV] += ysvij[2]*ysvij[3];
+                    rawSums[JB] += ysvij[0]*ysvij[4];
+                    rawSums[JS] += ysvij[1]*ysvij[4];
+                    rawSums[JV] += ysvij[2]*ysvij[4];
+                    rawSums[BB] += ysvij[0]*ysvij[0];
+                    rawSums[BS] += ysvij[0]*ysvij[1];
+                    rawSums[BV] += ysvij[0]*ysvij[2];
+                    rawSums[SS] += ysvij[1]*ysvij[1];
+                    rawSums[SV] += ysvij[0]*ysvij[2];
+                    rawSums[VV] += ysvij[2]*ysvij[2];
                 }
             }
-            centralMoments[B] = rawSums[B]/pixelCount;
-            centralMoments[S] = rawSums[S]/pixelCount;
-            centralMoments[V] = rawSums[V]/pixelCount;
-            centralMoments[XB] = (0.5 * centralMoments[B] 
-                - rawSums[XB]/pixelCount);
-            centralMoments[XS] = (0.5 * centralMoments[S] 
-                - rawSums[XS]/pixelCount);
-            centralMoments[XV] = (0.5 * centralMoments[V] 
-                - rawSums[XV]/pixelCount);
-            centralMoments[YB] = (0.5 * centralMoments[B] 
-                - rawSums[YB]/pixelCount);
-            centralMoments[YS] = (0.5 * centralMoments[S] 
-                - rawSums[YS]/pixelCount);
-            centralMoments[YV] = (0.5 * centralMoments[V] 
-                - rawSums[YV]/pixelCount);
+            centralMoments[B] = rawSums[B]/PIXELCOUNT;
+            centralMoments[S] = rawSums[S]/PIXELCOUNT;
+            centralMoments[V] = rawSums[V]/PIXELCOUNT;
+            centralMoments[IB] = (0.5 * centralMoments[B] 
+                - rawSums[IB]/PIXELCOUNT);
+            centralMoments[IS] = (0.5 * centralMoments[S] 
+                - rawSums[IS]/PIXELCOUNT);
+            centralMoments[IV] = (0.5 * centralMoments[V] 
+                - rawSums[IV]/PIXELCOUNT);
+            centralMoments[JB] = (0.5 * centralMoments[B] 
+                - rawSums[JB]/PIXELCOUNT);
+            centralMoments[JS] = (0.5 * centralMoments[S] 
+                - rawSums[JS]/PIXELCOUNT);
+            centralMoments[JV] = (0.5 * centralMoments[V] 
+                - rawSums[JV]/PIXELCOUNT);
             centralMoments[BB] = (centralMoments[B] * centralMoments[B] 
-                - rawSums[BB]/pixelCount);
+                - rawSums[BB]/PIXELCOUNT);
             centralMoments[BS] = (centralMoments[B] * centralMoments[S] 
-                - rawSums[BS]/pixelCount);
+                - rawSums[BS]/PIXELCOUNT);
             centralMoments[BS] = (centralMoments[B] * centralMoments[V] 
-                - rawSums[BV]/pixelCount);
+                - rawSums[BV]/PIXELCOUNT);
             centralMoments[SS] = (centralMoments[S] * centralMoments[S] 
-                - rawSums[SS]/pixelCount);
+                - rawSums[SS]/PIXELCOUNT);
             centralMoments[SV] = (centralMoments[S] * centralMoments[V] 
-                - rawSums[SV]/pixelCount);
+                - rawSums[SV]/PIXELCOUNT);
             centralMoments[VV] = (centralMoments[V] * centralMoments[V] 
-                - rawSums[VV]/pixelCount);
+                - rawSums[VV]/PIXELCOUNT);
             cookedMoments[B] = centralMoments[B];
             cookedMoments[S] = centralMoments[S];
             cookedMoments[V] = centralMoments[V];
@@ -131,24 +133,24 @@
                 centralMoments[BB]*centralMoments[VV])*0.5+0.5;
             cookedMoments[SV] = centralMoments[SV]/Math.sqrt(
                 centralMoments[SS]*centralMoments[VV])*0.5+0.5;
-            cookedMoments[XB] = centralMoments[XB]/Math.sqrt(
+            cookedMoments[IB] = centralMoments[IB]/Math.sqrt(
                 0.25*centralMoments[BB])*0.5+0.5;
-            cookedMoments[XS] = centralMoments[XS]/Math.sqrt(
+            cookedMoments[IS] = centralMoments[IS]/Math.sqrt(
                 0.25*centralMoments[SS])*0.5+0.5;
-            cookedMoments[XV] = centralMoments[XV]/Math.sqrt(
+            cookedMoments[IV] = centralMoments[IV]/Math.sqrt(
                 0.25*centralMoments[VV])*0.5+0.5; 
-            cookedMoments[YB] = centralMoments[YB]/Math.sqrt(
+            cookedMoments[JB] = centralMoments[JB]/Math.sqrt(
                 0.25*centralMoments[BB])*0.5+0.5;
-            cookedMoments[YS] = centralMoments[YS]/Math.sqrt(
+            cookedMoments[JS] = centralMoments[JS]/Math.sqrt(
                 0.25*centralMoments[SS])*0.5+0.5;
-            cookedMoments[YV] = centralMoments[YV]/Math.sqrt(
+            cookedMoments[JV] = centralMoments[JV]/Math.sqrt(
                 0.25*centralMoments[VV])*0.5+0.5;
             return cookedMoments;
-        }
+        };
         calc.nDims = 16;
         return calc;
     };
     // expose our module to the global object
-    global.video.Covariance = Covariance;
+    global.video.PluginMoments = PluginMoments;
     
 })( this, _, Rx );
