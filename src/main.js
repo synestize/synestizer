59f36659348fe2cb4ee38118c826800292ea272b
howthebodyworks <git@email.possumpalace.org>
2016-01-26 12:39:01 +0700

    correct webpack paths

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
+//Main entry point for whole app
+
 var Rx, React, ReactDOM, MidiIn, pureTabs;
 Rx = global.Rx = require('Rx');
 React = global.React = require('react');
 ReactDOM = global.React = require('react-dom');
-//MidiIn = global.MidiIn = require('./midi/midiin');
-//pureTabs = global.pureTabs = require('./vendor/puretabs/puretabs.js');
+pureTabs = global.pureTabs = require('./vendor/puretabs/puretabs.js');
+pureTabs.init('tabs-link', 'tabs-link-active');
 
-//pureTabs.init('tabs-link', 'tabs-link-active');
+//MidiIn = global.MidiIn = require('./midi/midiin');
 
 //MidiIn.init(document.getElementById("midi-input"));
 
