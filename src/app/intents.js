c80b754810389e6606cf8562293dbdd62597e8da
howthebodyworks <git@email.possumpalace.org>
2016-02-26 16:49:56 +0700

    Not using the "master" app

--- src/app/intents.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-//nothing for now
--- src/app/views.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-//Nothing, for now. We render panels ad-hoc in main.js
-//slightly impure app style, but much more convenientfor now.
