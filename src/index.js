d7dd184be95c6be4de9bb5b4fda0ed917c858588
howthebodyworks <git@email.possumpalace.org>
2016-03-26 14:36:14 +1100

    full redux refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 //Main entry point for whole app
 "use strict";
 
+let React = require('react');
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 var Rx = global.Rx = require('Rx');
 //debug mode:
 Rx.config.longStackSupport = true;
