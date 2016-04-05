d89a039cdc251369a512e6025af693b7c3c90e4a
howthebodyworks <git@email.possumpalace.org>
2016-04-05 21:07:15 +1000

    rough state display

--- src/audio_master/views.js
+++ src/audio_master/views.js
@@ -9,7 +9,7 @@ var SynthControls = function(props) {
   return (<div className="audiocontrolset">
     <h2>Master Controls</h2>
     <MasterGain
-      value={props.mastergain}
+      value={props.masterGain}
       action={intents.setMasterGain} />
     <GenericAudioParam
       label="Tempo"
@@ -22,33 +22,43 @@ var SynthControls = function(props) {
   </div>)
 };
 var MasterGain = function({value, action}) {
-  return (<div className="paramControl mastergain">
-    <label className="paramLabel" htmlFor="masterGainSlider">
+  return (<div className="param-control mastergain">
+    <label className="label" htmlFor="master-gain-slider">
       Gain
     </label>
-    <input className="paramSlider" id="masterGainSlider" type="range" value={value} onChange={(ev)=>action(ev.target.value)} min="-40" max="6" step="1" className="paramValue" />
-    <span>{value}</span>
+    <div className="slider-wrap">
+      <input className="slider value"
+        id="master-gain-slider"
+        type="range"
+        value={value}
+        onChange={(ev)=>action(ev.target.value)}
+        min="-40" max="6" step="1" />
+    </div>
+    <span className="mapped-value">{value} dB</span>
   </div>)
 };
 var GenericAudioParam = function({median, perturbation, actual, address, label, action}) {
-  return (<div className="paramControl {address}">
-    <label className="paramLabel"
+  //console.debug("Styleface", actual, transform.bipolPerc(actual || 0.0));
+  let divStyle = {
+    width: transform.bipolPerc(actual || 0.0)+"%"
+  };
+  return (<div className={"param-control " + address} >
+    <label className="label"
       htmlFor={address + "-slider"}>
       {label}
     </label>
-    <input className="paramSlider paramValue"
-      id={address + "-slider"}
-      type="range"
-      value={median}
-      onChange={(ev)=>action(ev.target.value)}
-      min="-1" max="1" step="any" />
-    <span>{actual.toFixed(2)}</span>
-    <input className="paramSlider actual"
-      id={address + "-slider-actual"}
-      type="range"
-      value={actual}
-      min="-1" max="1" step="any"
-      disabled={true} />
+    <div className="slider-wrap">
+      <input className="slider value"
+        id={address + "-slider"}
+        type="range"
+        value={median}
+        onChange={(ev)=>action(ev.target.value)}
+        min="-1" max="1" step="any" />
+      <div className="state-bar"
+        id={address + "-slider-actual"}
+        style={divStyle} />
+    </div>
+    <span className="mapped-value">{actual.toFixed(2)}</span>
   </div>)
 };
 
