386f77f21fa8dbc872bbc27e74c48ccd05aa6dde
howthebodyworks <git@email.possumpalace.org>
2016-06-13 21:39:28 +1000

    sketch of RX-redux jack

---
+++ src/lib/rx_redux.js
@@ -0,0 +1,10 @@
+// https://github.com/reactjs/redux/issues/303#issuecomment-125184409
+export function toObservable(store) {
+  return {
+    subscribe({ onNext }) {
+      let dispose = store.subscribe(() => onNext(store.getState()));
+      onNext(store.getState());
+      return { dispose };
+    }
+  }
+}
