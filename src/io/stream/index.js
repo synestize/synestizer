09beac6fdefdda2d1b353c797fee37121347e9cb
howthebodyworks <git@email.possumpalace.org>
2016-07-19 17:31:16 +1000

    devtools

--- src/io/stream/index.js
+++ src/io/stream/index.js
@@ -4,6 +4,9 @@ import setop from '../../lib/setop.js'
 import transform from '../../lib/transform.js'
 import { addSourceStream, removeSourceStream, setSourceStreamValue, setAllSourceStreamValues, addSinkStream, removeSinkStream, setSinkStreamValue, setAllSinkStreamValue, setSourceSinkScale, setSinkBias } from '../../actions/stream'
 import { toObservable } from '../../lib/rx_redux'
+import React from 'react'
+
+typeof window !== "undefined" && (window.React = React); // for devtools
 
 /*
 internal state handles high-speed source updates and slower sink updates
