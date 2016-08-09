c63e25e816d2d138c32a1973f2267254f454d4fc
howthebodyworks <git@email.possumpalace.org>
2016-08-09 11:53:55 +0800

    purge empty headers

--- src/components/SubPane.js
+++ src/components/SubPane.js
@@ -1,12 +1,18 @@
 "use strict";
 import React, { Component, PropTypes } from 'react'
 
-const SubPane = ({name, title, children}) => (
-  <section className={"subpane " + name}>
-    <h2>{title}</h2>
-    {children}
-  </section>
-)
+const SubPane = ({name, title, children}) => {
+  let titleBlock = "";
+  if (title) {
+    titleBlock= (<h2>{title}</h2>)
+  }
+  return (
+    <section className={"subpane " + name}>
+      {titleBlock}
+      {children}
+    </section>
+  )
+}
 SubPane.propTypes = {
   name: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
