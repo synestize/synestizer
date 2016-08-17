ae0ad0463b3c2bdb9250e52c4d8eed9c8673074a
howthebodyworks <git@email.possumpalace.org>
2016-08-18 00:10:16 +1000

    revised audio control scheme

--- src/actions/audio.js
+++ src/actions/audio.js
@@ -6,8 +6,8 @@ import { addSourceSignal, removeSourceSignal, addSinkSignal, removeSinkSignal } 
 export const SET_ALL_AUDIO_SOURCE_DEVICES = 'SET_ALL_AUDIO_SOURCE_DEVICES'
 export const SET_AUDIO_SOURCE_DEVICE = 'SET_AUDIO_SOURCE_DEVICE'
 export const SET_VALID_AUDIO_SOURCE_DEVICE = 'SET_VALID_AUDIO_SOURCE_DEVICE'
-export const PUBLISH_AUDIO_SOURCE_CONTROL = 'PUBLISH_AUDIO_SOURCE_CONTROL'
-export const UNPUBLISH_AUDIO_SOURCE_CONTROL = 'UNPUBLISH_AUDIO_SOURCE_CONTROL'
+export const PUBLISH_AUDIO_SOURCE_SIGNAL = 'PUBLISH_AUDIO_SOURCE_SIGNAL'
+export const UNPUBLISH_AUDIO_SOURCE_SIGNAL = 'UNPUBLISH_AUDIO_SOURCE_SIGNAL'
 export const SET_AUDIO_SOURCE_CONTROL_NOMINAL_VALUE = 'SET_AUDIO_SOURCE_CONTROL_NOMINAL_VALUE'
 
 export const SET_ALL_AUDIO_SINK_DEVICES = 'SET_ALL_AUDIO_SINK_DEVICES'
@@ -15,9 +15,11 @@ export const SET_AUDIO_SINK_DEVICE = 'SET_AUDIO_SINK_DEVICE'
 export const SET_VALID_AUDIO_SINK_DEVICE = 'SET_VALID_AUDIO_SINK_DEVICE'
 export const ADD_AUDIO_SINK_CONTROL = 'ADD_AUDIO_SINK_CONTROL'
 export const REMOVE_AUDIO_SINK_CONTROL = 'REMOVE_AUDIO_SINK_CONTROL'
-export const PUBLISH_AUDIO_SINK_CONTROL = 'PUBLISH_AUDIO_SINK_CONTROL'
-export const UNPUBLISH_AUDIO_SINK_CONTROL = 'UNPUBLISH_AUDIO_SINK_CONTROL'
+export const PUBLISH_AUDIO_SINK_SIGNAL = 'PUBLISH_AUDIO_SINK_SIGNAL'
+export const UNPUBLISH_AUDIO_SINK_SIGNAL = 'UNPUBLISH_AUDIO_SINK_SIGNAL'
 export const SET_AUDIO_SINK_CONTROL_BIAS = 'SET_AUDIO_SINK_CONTROL_BIAS'
+export const SET_AUDIO_SINK_CONTROL_SCALE = 'SET_AUDIO_SINK_CONTROL_SCALE'
+export const SET_AUDIO_SINK_CONTROL_SIGNAL = 'SET_AUDIO_SINK_CONTROL_SIGNAL'
 export const ADD_ENSEMBLE = 'ADD_ENSEMBLE'
 /*
  * action creators
@@ -40,14 +42,11 @@ export function setAudioSinkDevice(sourceId) {
 export function setValidAudioSinkDevice(yn) {
  return { type: SET_VALID_AUDIO_SINK_DEVICE, payload: yn }
 }
-export function publishAudioSourceControl(key) {
- return { type: PUBLISH_AUDIO_SOURCE_CONTROL, payload: key }
+export function publishAudioSourceSignal(key) {
+ return { type: PUBLISH_AUDIO_SOURCE_SIGNAL, payload: key }
 }
-export function unpublishAudioSourceControl(key) {
- return { type: UNPUBLISH_AUDIO_SOURCE_CONTROL, payload: key }
-}
-export function setAudioSourceControlNominalValue(key, val) {
- return { type: SET_AUDIO_SOURCE_CONTROL_NOMINAL_VALUE, payload: {key, val}}
+export function unpublishAudioSourceSignal(key) {
+ return { type: UNPUBLISH_AUDIO_SOURCE_SIGNAL, payload: key }
 }
 export function addAudioSinkControl(meta) {
  return { type: ADD_AUDIO_SINK_CONTROL, payload: meta }
@@ -55,14 +54,20 @@ export function addAudioSinkControl(meta) {
 export function removeAudioSinkControl(key) {
  return { type: REMOVE_AUDIO_SINK_CONTROL, payload: key }
 }
-export function publishAudioSinkControl(key) {
- return { type: PUBLISH_AUDIO_SINK_CONTROL, payload: key }
+export function publishAudioSinkSignal(key) {
+ return { type: PUBLISH_AUDIO_SINK_SIGNAL, payload: key }
 }
-export function unpublishAudioSinkControl(key) {
- return { type: UNPUBLISH_AUDIO_SINK_CONTROL, payload: key }
+export function unpublishAudioSinkSignal(key) {
+ return { type: UNPUBLISH_AUDIO_SINK_SIGNAL, payload: key }
 }
-export function setAudioSinkControlNominalValue(key, val) {
+export function setAudioSinkControlBias(key, val) {
  return { type: SET_AUDIO_SINK_CONTROL_BIAS, payload: {key, val}}
+}
+export function setAudioSinkControlScale(key, val) {
+ return { type: SET_AUDIO_SINK_CONTROL_SCALE, payload: {key, val}}
+}
+export function setAudioSinkControlSignal(key, val) {
+ return { type: SET_AUDIO_SINK_CONTROL_SIGNAL, payload: {key, val}}
 }
 export function addEnsemble(meta) {
   return { type: ADD_ENSEMBLE, payload: meta }
--- src/reducers/audio.js
+++ src/reducers/audio.js
@@ -2,15 +2,17 @@ import { combineReducers } from 'redux'
 
 import {
   SET_AUDIO_SOURCE_DEVICE,
-  PUBLISH_AUDIO_SOURCE_CONTROL,
-  UNPUBLISH_AUDIO_SOURCE_CONTROL,
+  PUBLISH_AUDIO_SOURCE_SIGNAL,
+  UNPUBLISH_AUDIO_SOURCE_SIGNAL,
   SET_AUDIO_SOURCE_CONTROL,
   SET_AUDIO_SINK_DEVICE,
   ADD_AUDIO_SINK_CONTROL,
   REMOVE_AUDIO_SINK_CONTROL,
-  PUBLISH_AUDIO_SINK_CONTROL,
-  UNPUBLISH_AUDIO_SINK_CONTROL,
+  PUBLISH_AUDIO_SINK_SIGNAL,
+  UNPUBLISH_AUDIO_SINK_SIGNAL,
   SET_AUDIO_SINK_CONTROL_BIAS,
+  SET_AUDIO_SINK_CONTROL_SCALE,
+  SET_AUDIO_SINK_CONTROL_SIGNAL,
   ADD_ENSEMBLE
 } from '../actions/audio'
 
@@ -26,10 +28,10 @@ export function sourceDevice(state="default", {type, payload}) {
 export function sourceControls(state={}, {type, payload}) {
   let next = {...state}
   switch (type) {
-    case PUBLISH_AUDIO_SOURCE_CONTROL:
+    case PUBLISH_AUDIO_SOURCE_SIGNAL:
       next[payload] = 0.0
       break
-    case UNPUBLISH_AUDIO_SOURCE_CONTROL:
+    case UNPUBLISH_AUDIO_SOURCE_SIGNAL:
       delete next[payload]
   }
   return next
@@ -44,37 +46,52 @@ export function sinkDevice(state="default", {type, payload}) {
   }
 }
 
-export function sinkControlBias(state={}, {type, payload}) {
-  let next = state;
-  switch (type) {
-    case ADD_AUDIO_SINK_CONTROL:
-      next = {...state}
-      next[payload.key] = 0.0
-      break
-    case REMOVE_AUDIO_SINK_CONTROL:
-      next = {...state}
-      delete next[payload.key]
-    case SET_AUDIO_SINK_CONTROL_BIAS:
-      next = {...state}
-      next[payload.key] = payload.val
-  }
-  return next
-}
-
-export function sinkControlMeta(state={}, {type, payload}) {
+export function sinkControls(state={}, {type, payload}) {
   let next = state;
   switch (type) {
     case ADD_AUDIO_SINK_CONTROL:
       next = {...state}
       next[payload.key] = payload
-      break
+      return next
     case REMOVE_AUDIO_SINK_CONTROL:
       next = {...state}
-      delete next[payload.key]
+      delete next[payload]
+      return next
+    case SET_AUDIO_SINK_CONTROL_BIAS:
+    case SET_AUDIO_SINK_CONTROL_SCALE:
+      let {key, val} = payload;
+      next = {...state}
+      next[key] = _sinkControl(next[key], {type, payload})
+      return next
+    default:
+      return state
+  }
+}
+
+//Only individual controls are edited here
+export function _sinkControl(state={}, {type, payload}) {
+  let next = {...state};
+  let {key, val} = payload;
+  switch (type) {
+    case SET_AUDIO_SINK_CONTROL_BIAS:
+      next.bias = val
+      break
+    case SET_AUDIO_SINK_CONTROL_SCALE:
+      next.scale = val
+      break
   }
   return next
 }
 
+export function sinkControlSignal(state={}, {type, payload}) {
+  switch (type) {
+    case SET_AUDIO_SINK_CONTROL_SIGNAL:
+      let next = {...state};
+      let {key, val} = payload;
+      next[val] = key
+      return next
+  }
+}
 function ensembles(state={}, {type, payload}) {
   let next = state;
   switch (type) {
@@ -90,7 +107,7 @@ const audio = combineReducers({
    sourceDevice,
   //  sourceControlVals,
    sinkDevice,
-   sinkControlBias,
+   sinkControls,
 })
 
 export default audio
