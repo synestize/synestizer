8444622d5a1e39b987ea7ba1edaa0a0cd7c6988d
howthebodyworks <git@email.possumpalace.org>
2016-08-23 23:42:51 +1000

    paletable audio param

--- src/components/SoundPane.js
+++ src/components/SoundPane.js
@@ -3,7 +3,8 @@ import ControlTriad from '../containers/ControlTriad';
 import React, { Component, PropTypes, Children } from 'react';
 
 const SoundPane = ({version}) => (<Pane paneId="audio-pane" version={version}>
-<ControlTriad />
+<p>coming soon </p>
+{ /* <ControlTriad /> */ }
 </Pane>)
 
 export default SoundPane;
--- src/io/signal.js
+++ src/io/signal.js
@@ -15,8 +15,8 @@ import {
 import { toObservable } from '../lib/rx_redux'
 import React from 'react'
 
-const SIGNAL_RATE = 100;
-const UI_RATE = 5000;
+const SIGNAL_RATE = 50;
+const UI_RATE = 50;
 /*
 internal state handles high-speed source updates and periodic sink updates and UI updates
 */
