0be80de2700385ae827605ea129e2556e0e20a94
howthebodyworks <git@email.possumpalace.org>
2016-09-21 22:02:33 +1000

    brute-force synth param updates

--- src/io/audio.js
+++ src/io/audio.js
@@ -222,27 +222,22 @@ export default function init(store, signalio) {
   Observable::combineLatest(
     storeStream.pluck(
       'audio', 'master', 'gain'
-    ).distinctUntilChanged(),
+    ),
     storeStream.pluck(
       'audio', 'master', 'mute'
-    ).distinctUntilChanged(),
-    storeStream.pluck(
-      '__volatile', 'audio', 'audioReady'
-    ).distinctUntilChanged()
+    ),
   ).subscribe(
-    ([gain, mute, ready]) => {
-      console.debug('gm', gain, mute, ready);
+    ([gain, mute]) => {
+      console.debug('gm', gain, mute);
       //I'm sure there is a more "RX" way of doing this.
-      if (ready){
-        Tone.Master.mute = mute;
-        if (!mute) Tone.Master.volume.rampTo(gain, 0.1)
-      }
+      Tone.Master.mute = mute;
+      if (!mute) Tone.Master.volume.rampTo(gain, 0.1)
     }
   );
 
   storeStream.pluck(
     'audio', 'master', 'tempo'
-  ).distinctUntilChanged().subscribe((bpm)=>{
+  ).subscribe((bpm)=>{
     Tone.Transport.bpm.rampTo(bpm, 0.1)
   });
   return audioInfrastructure
