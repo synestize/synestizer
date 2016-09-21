a90335061fd9cec463915daa0fde406102559c07
howthebodyworks <git@email.possumpalace.org>
2016-09-21 21:49:11 +1000

    adaptive settings

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,7 @@
-//const SIGNAL_RATE = 1000/25;
-//const UI_RATE = 100;
-const SIGNAL_RATE = 2000;
-const UI_RATE = 2000;
+export let SIGNAL_RATE = 2000;
+export let UI_RATE = 2000;
+
+if (PRODUCTION){
+  SIGNAL_RATE = 1000/25;
+  UI_RATE = 100;
+}
