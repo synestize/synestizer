6df2c24ba1456019d26c6efdbbc6007d2b3210b4
howthebodyworks <git@email.possumpalace.org>
2016-09-26 18:20:06 +1000

    fix spurious data in ControlTriad

--- src/containers/ControlTriad.js
+++ src/containers/ControlTriad.js
@@ -4,28 +4,11 @@ import { union, difference, intersection } from '../lib/collections'
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    currentNum: ownProps.cc,
-    ccset: ownProps.ccset,
-    soloed: ownProps.solocc===ownProps.cc
   }
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  const disp = {
-    onChange: (ev) => {
-      dispatch(ownProps.adder(ev))
-      dispatch(ownProps.remover(ownProps.cc))
-    },
-    onDelete: () => {
-      dispatch(ownProps.remover(ownProps.cc))
-    },
-  }
-  if (typeof ownProps.solotoggler==='function') {
-    disp.onSolo = () => {
-      dispatch(ownProps.solotoggler(ownProps.cc))
-    }
-  }
-  return disp
+  return {}
 };
 
 const ControlTriad = connect(
