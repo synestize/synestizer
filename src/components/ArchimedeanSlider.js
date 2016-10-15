cb62e61ef4f0be9e86ec0239b81691467bab860a
howthebodyworks <git@email.possumpalace.org>
2016-10-15 16:55:37 +1100

    smells like teen slide

--- src/components/ArchimedeanSlider.js
+++ src/components/ArchimedeanSlider.js
@@ -7,35 +7,57 @@ const ArchimedeanSlider = ({
   value=0,
   perturb=0,
   className="",
-  height=100,
-  width=200,
+  width=256,
+  height=96,
   onScaleChange,
   onBiasChange,
-  fill
+  biasFill="brown", // move this to CSS?
+  scaleFill="red",
+  trackFill="gray",
+  biasThumbFill="black"
 }) => {
+  const midX = width/2;
+  const midY = height/2;
+  const thumbSize = midY/2;
+  const trackLeft = width/8;
+  const trackRight = width - trackLeft;
+  const trackHeight = height/8;
+  const trackLen = trackRight - trackLeft;
+  const trackMidY = 3*height/4;
+  const biasThumbX = midX + trackLen /2 * bias;
+  const valueThumbX = midX + trackLen /2 * value;
 
   return (<div className={"param-control " + className} >
       <svg
-          width="100%"
-          height="100%"
+          width={width}
+          height={height}
           xmlns="http://www.w3.org/2000/svg"
           xmlnsXlink="http://www.w3.org/1999/xlink"
       >
         <style>
-            { `.classA { fill:${fill} }` }
+            { `.track { fill:${trackFill} };
+            .biasThumb { fill:${biasThumbFill}` }
         </style>
-        <defs>
-            <g id="Port">
-                <circle style={{fill:'inherit'}} r="10"/>
-            </g>
-        </defs>
-
-        <text y="15">black</text>
-        <use x="70" y="10" xlinkHref="#Port" />
-        <text y="35">{ fill }</text>
-        <use x="70" y="30" xlinkHref="#Port" className="classA"/>
-        <text y="55">blue</text>
-        <use x="0" y="50" xlinkHref="#Port" style={{fill:'blue'}}/>
+        <rect
+          x={trackLeft}
+          y={trackMidY-trackHeight/2}
+          width={trackLen}
+          height={trackHeight} />
+        <line
+          x1={midX}
+          x2={midX}
+          y1={midY}
+          y2={height}
+          stroke="orange"
+          fill="transparent" stroke-width="2"/>
+        <rect
+          x={trackLeft}
+          y={trackMidY-trackHeight/2}
+          width={trackLen}
+          height={trackHeight} />
+        <circle cx={biasThumbX} cy={trackMidY} r={thumbSize} />
+        <polygon points="50 160, 55 180, 70 180, 60 190, 65 205, 50 195, 35 205, 40 190, 30 180, 45 180"/>
+        <path className="" d='M0 24 L0 40 L64 32 Z'  style={{fill:'blue'}}/>
       </svg>
     {bias}/{scale}/{value}/{perturb}
   </div>)
