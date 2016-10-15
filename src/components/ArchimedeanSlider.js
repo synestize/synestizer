3e6e3c7ab63fb79c5251f1f8765eaaa8b55fecea
howthebodyworks <git@email.possumpalace.org>
2016-10-15 15:28:06 +1100

    example SVG

--- src/components/ArchimedeanSlider.js
+++ src/components/ArchimedeanSlider.js
@@ -10,10 +10,33 @@ const ArchimedeanSlider = ({
   height=100,
   width=200,
   onScaleChange,
-  onBiasChange
+  onBiasChange,
+  fill
 }) => {
 
   return (<div className={"param-control " + className} >
+      <svg
+          width="100%"
+          height="100%"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <style>
+            { `.classA { fill:${fill} }` }
+        </style>
+        <defs>
+            <g id="Port">
+                <circle style={{fill:'inherit'}} r="10"/>
+            </g>
+        </defs>
+
+        <text y="15">black</text>
+        <use x="70" y="10" xlinkHref="#Port" />
+        <text y="35">{ fill }</text>
+        <use x="70" y="30" xlinkHref="#Port" className="classA"/>
+        <text y="55">blue</text>
+        <use x="0" y="50" xlinkHref="#Port" style={{fill:'blue'}}/>
+      </svg>
     {bias}/{scale}/{value}/{perturb}
   </div>)
 };
