e6d9b98416df965920c23efce0e572b31501aca1
howthebodyworks <git@email.possumpalace.org>
2016-10-16 12:24:01 +1100

    scale slider arrow

--- src/components/ScaleSlider.js
+++ src/components/ScaleSlider.js
@@ -2,40 +2,49 @@ import React, { Component, PropTypes, Children } from 'react';
 import {bipolPerc} from '../lib/transform'
 
 const ScaleSlider = ({
-  scale=0,
+  scale=0.5,
   perturb=0,
   className="",
-  width=64,
-  height=36,
-  onChange,
-  scaleFill="red",
+  width=80,
+  height=32,
+  onChange=()=>null,
+  scaleArrowFill="blue",
+  scaleBackingFill="black",
+  tickColor="orange",
   x=0,
   y=0
 }) => {
   const midX = width/2;
   const midY = height/2;
-  const thumbSize = midY/2;
-  const trackLeft = width/8;
-  const trackRight = width - trackLeft;
-  const trackHeight = height/8;
-  const trackLen = trackRight - trackLeft;
-  const trackMidY = 3*height/4;
-  const biasThumbX = midX + trackLen /2 * bias;
-  const valueThumbX = midX + trackLen /2 * value;
-
-  return (<div className={"param-control " + className} >
-    <svg
+  const left = midX - scale * midX;
+  const right = midX + scale * midX;
+  console.debug('pol', `${left} 0, ${right} ${midY}, ${left} ${height}`)
+  return (<svg
         width={width}
         height={height}
         xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-    >
+        xmlnsXlink="http://www.w3.org/1999/xlink" >
       <style>
-          { `.track { fill: ${trackFill} };` }
+          { `.backing { fill: ${scaleBackingFill} };` }
       </style>
-      <polygon points={`50 160, 55 180, 70 180, 60 190, 65 205, 50 195, 35 205, 40 190, 30 180, 45 180`} style={{fill:'blue'}} transform={`translate(${x},${y})`}/>
+      <rect
+        x={0}
+        y={0}
+        width={width}
+        height={height}
+        className="backing" />
+      <line
+        x1={midX}
+        x2={midX}
+        y1={0}
+        y2={height}
+        stroke={tickColor}
+        fill="transparent"
+        strokeWidth="2"/>
+      <polygon points={`${left} 0, ${right} ${midY}, ${left} ${height}`} style={{fill:scaleArrowFill}} transform={`translate(${x},${y})`}
+      />
     </svg>
-  </div>)
+  )
 };
 
 ScaleSlider.propTypes = {
@@ -43,7 +52,12 @@ ScaleSlider.propTypes = {
   scale: PropTypes.number,
   value: PropTypes.number,
   perturb: PropTypes.number,
-  onScaleChange: PropTypes.func.isRequired,
+  x: PropTypes.string,
+  y: PropTypes.string,
+  onChange: PropTypes.func,
+  scaleArrowFill: PropTypes.string,
+  scaleBackingFill: PropTypes.string,
+  className: PropTypes.string,
 }
 
 export default ScaleSlider
