503113ad8f9d59cea5845cb11143081ee688577c
howthebodyworks <git@email.possumpalace.org>
2016-10-16 19:01:55 +1100

    shonky mutable data in App class

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,31 @@ import {bipolPerc} from '../lib/transform'
 import ArchimedeanSlider from './ArchimedeanSlider'
 import ScaleSlider from './ScaleSlider'
 
-const App = ({}) => {
-  return (<div className="meh" >
-    <ScaleSlider
-      height={40}
-      width={150} />
-    <ArchimedeanSlider
-      className="test-class-1"
-      bias={0.5}
-      scale={0.5}
-      value={0.75}
-      perturb={-0.25}
-      onBiasChange={()=>null}
-      onScaleChange={()=>null}
-      height={50}
-      width={200} />
-  </div>)
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      bias: 0.5,
+      scale: 0.5,
+      value: 0.75,
+      perturb: -0.25
+    }
+  }
+  render = ()=> {
+    return (<div className="meh" >
+      <ArchimedeanSlider
+        className="test-class-1"
+        onBiasChange={()=>null}
+        onBiasDoubleClick={()=>{
+          this.setState({'bias': 0.0})
+        }}
+        onScaleChange={()=>null}
+        onScaleDoubleClick={()=>null}
+        height={50}
+        width={200}
+        {...this.state} />
+    </div>)
+  }
 };
 
 App.propTypes = {
