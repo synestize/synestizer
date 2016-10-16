017526ab6737bc4c3024eba20a0a6647a71509d9
howthebodyworks <git@email.possumpalace.org>
2016-10-16 16:31:16 +1100

    convert ArchimedeanSlider to be a class

--- src/components/ArchimedeanSliderSVG.js
+++ src/components/ArchimedeanSliderSVG.js
@@ -2,107 +2,110 @@ import React, { Component, PropTypes, Children } from 'react';
 import {bipolPerc} from '../lib/transform'
 import ScaleSliderSVG from './ScaleSliderSVG'
 
-const ArchimedeanSliderSVG = ({
-  bias=0,
-  scale=0,
-  value=0,
-  perturb=0,
-  className='',
-  width=256,
-  height=96,
-  onScaleChange,
-  onBiasChange,
-  biasFill='brown', // move this to CSS?
-  trackFill='gray',
-  scaleArrowFill='blue',
-  scaleBackingFill='black',
-  perturbArrowFill='rgba(0,0,255,0.8)',
-  biasThumbFill='black',
-  tickColor='orange',
-  biasBackingFill='white',
-  binderColor='orange',
-  transform='',
-  actualColor='orange'
-}) => {
-  const biasHeight = 2* height/3;
-  const biasTop = height-biasHeight;
-  const biasMid = (biasHeight + height)/2;
-  const midX = width/2;
+class ArchimedeanSliderSVG extends React.Component{
+  render = () => {
+    let {
+      bias=0,
+      scale=0,
+      value=0,
+      perturb=0,
+      className='',
+      width=256,
+      height=96,
+      onScaleChange,
+      onBiasChange,
+      biasFill='brown', // move this to CSS?
+      trackFill='gray',
+      scaleArrowFill='blue',
+      scaleBackingFill='black',
+      perturbArrowFill='rgba(0,0,255,0.8)',
+      biasThumbFill='black',
+      tickColor='orange',
+      biasBackingFill='white',
+      binderColor='orange',
+      transform='',
+      actualColor='orange'
+    } = this.props;
+    const biasHeight = 2* height/3;
+    const biasTop = height-biasHeight;
+    const biasMid = (biasHeight + height)/2;
+    const midX = width/2;
 
-  const thumbSize = biasHeight/4;
-  const trackHeight = biasHeight/5;
-  const trackLeft = thumbSize;
-  const trackRight = width - trackLeft;
-  const trackLen = trackRight - trackLeft;
-  const trackMidY = biasTop + biasHeight/2;
-  const biasThumbX = midX + trackLen /2 * bias;
-  const valueThumbX = midX + trackLen /2 * value;
+    const thumbSize = biasHeight/4;
+    const trackHeight = biasHeight/5;
+    const trackLeft = thumbSize;
+    const trackRight = width - trackLeft;
+    const trackLen = trackRight - trackLeft;
+    const trackMidY = biasTop + biasHeight/2;
+    const biasThumbX = midX + trackLen /2 * bias;
+    const valueThumbX = midX + trackLen /2 * value;
 
-  const scaleHeight = height - biasHeight;
-  const scaleWidth = width/2;
-  const scaleLeft = (width - scaleWidth) * (1 + scale) / 2;
-  const scaleMidX = scaleLeft + scaleWidth/2;
-  const scaleMidY = scaleHeight/2;
-  return (
-    <g transform={transform}>
-      <g>
-        <style>
-            { `.track { fill:${trackFill} };
-            .biasThumb { fill:${biasThumbFill} ;
-              cursor: move;}
-            };` }
-        </style>
-        <rect
-          x={0}
-          y={biasTop}
-          width={width}
-          height={biasHeight}
-          fill={biasBackingFill}
-        />
-        <rect
-          x={trackLeft}
-          y={trackMidY-trackHeight/2}
-          width={trackLen}
-          height={trackHeight} />
-        <line
-          x1={midX}
-          x2={midX}
-          y1={biasTop}
-          y2={height}
-          stroke={tickColor}
-          fill='transparent'
-          strokeWidth='2'/>
-        <circle cx={biasThumbX} cy={trackMidY} r={thumbSize} />
+    const scaleHeight = height - biasHeight;
+    const scaleWidth = width/2;
+    const scaleLeft = (width - scaleWidth) * (1 + scale) / 2;
+    const scaleMidX = scaleLeft + scaleWidth/2;
+    const scaleMidY = scaleHeight/2;
+
+    let c = (<g transform={transform}>
+        <g>
+          <style>
+              { `.track { fill:${trackFill} };
+              .biasThumb { fill:${biasThumbFill} ;
+                cursor: move;}
+              };` }
+          </style>
+          <rect
+            x={0}
+            y={biasTop}
+            width={width}
+            height={biasHeight}
+            fill={biasBackingFill}
+          />
+          <rect
+            x={trackLeft}
+            y={trackMidY-trackHeight/2}
+            width={trackLen}
+            height={trackHeight} />
+          <line
+            x1={midX}
+            x2={midX}
+            y1={biasTop}
+            y2={height}
+            stroke={tickColor}
+            fill='transparent'
+            strokeWidth='2'/>
+          <circle cx={biasThumbX} cy={trackMidY} r={thumbSize} />
+          <line
+            x1={biasThumbX}
+            x2={valueThumbX}
+            y1={trackMidY}
+            y2={trackMidY}
+            stroke={actualColor}
+            fill='transparent'
+            strokeWidth={trackHeight} />
+        </g>
+        <ScaleSliderSVG scale={scale}
+          perturb={perturb}
+          width={scaleWidth}
+          height={scaleHeight}
+          onChange={onScaleChange}
+          scaleArrowFill={scaleArrowFill}
+          scaleBackingFill={scaleBackingFill}
+          perturbArrowFill={perturbArrowFill}
+          transform={`translate(${scaleLeft},${0})`} />
         <line
           x1={biasThumbX}
-          x2={valueThumbX}
+          x2={scaleMidX}
           y1={trackMidY}
-          y2={trackMidY}
-          stroke={actualColor}
+          y2={scaleMidY}
+          stroke={binderColor}
           fill='transparent'
-          strokeWidth={trackHeight} />
-      </g>
-      <ScaleSliderSVG scale={scale}
-        perturb={perturb}
-        width={scaleWidth}
-        height={scaleHeight}
-        onChange={onScaleChange}
-        scaleArrowFill={scaleArrowFill}
-        scaleBackingFill={scaleBackingFill}
-        perturbArrowFill={perturbArrowFill}
-        transform={`translate(${scaleLeft},${0})`} />
-      <line
-        x1={biasThumbX}
-        x2={scaleMidX}
-        y1={trackMidY}
-        y2={scaleMidY}
-        stroke={binderColor}
-        fill='transparent'
-        strokeWidth='2'/>
-    </g>
-  )
-};
-/*  */
+          strokeWidth='2'/>
+      </g>)
+    return c
+  };
+}
+
 ArchimedeanSliderSVG.propTypes = {
   bias: PropTypes.number,
   scale: PropTypes.number,
@@ -114,4 +117,4 @@ ArchimedeanSliderSVG.propTypes = {
   transform: PropTypes.string,
 }
 
-export default ArchimedeanSliderSVG
+export default ArchimedeanSliderSVG;
