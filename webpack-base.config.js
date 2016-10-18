fb3bdef8dd141a3b78be227edbea270c6c4cb809
howthebodyworks <git@email.possumpalace.org>
2016-10-18 22:59:14 +1100

    simple dev mode

---
+++ webpack-base.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    //'webpack/hot/dev-server' ,
+    './src/index.js'
+  ],
+  output: {
+    publicPath: 'js/',
+    path: path.join(__dirname, 'js'),
+    filename: 'bundle.js'
+  },
+  devtool: 'eval',
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'source-map-loader'
+      }
+    ],
+    noParse: /node_modules\/localforage\/dist\/localforage.js/,
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel',
+      query: {
+        cacheDirectory: '/tmp',
+        "presets": [
+          "es2015",
+          "react",
+          "stage-0",
+          // "escompress"
+        ],
+        "plugins": [
+          // "transform-es2015-destructuring",
+          // // "transform-es2015-spread",
+          // "transform-dead-code-elimination",
+          // "transform-object-rest-spread",
+          "transform-class-properties",
+        ]
+      }
+    }]
+  },
+  resolve: {
+    modulesDirectories: [path.join(__dirname, 'src'), 'node_modules']
+  },
+  plugins: [
+  ]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,15 @@
 var path = require('path');
 var webpack = require('webpack');
+var OfflinePlugin = require('offline-plugin');
 
-module.exports = {
-  entry: [
-    //'webpack/hot/dev-server' ,
-    './src/index.js'
-  ],
-  output: {
-    publicPath: 'js/',
-    path: path.join(__dirname, 'js'),
-    filename: 'bundle.js'
-  },
-  devtool: 'eval',
-  module: {
-    preLoaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        loader: 'source-map-loader'
-      }
-    ],
-    noParse: /node_modules\/localforage\/dist\/localforage.js/,
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /(node_modules)/,
-      loader: 'babel',
-      query: {
-        cacheDirectory: '/tmp',
-        "presets": [
-          "es2015",
-          "react",
-          "stage-0",
-          // "escompress"
-        ],
-        "plugins": [
-          // "transform-es2015-destructuring",
-          // // "transform-es2015-spread",
-          // "transform-dead-code-elimination",
-          // "transform-object-rest-spread",
-          "transform-class-properties",
-        ]
-      }
-    }]
-  },
-  resolve: {
-    modulesDirectories: [path.join(__dirname, 'src'), 'node_modules']
-  },
-  plugins: [
-  ]
-};
+var config = require("./webpack-base.config.js");
+config.plugins.push(new webpack.DefinePlugin({
+  PRODUCTION: JSON.stringify(false),
+  "process.env": {
+    NODE_ENV: JSON.stringify("development")
+  }
+}))
+
+delete(config.devtool)
+
+module.exports = config
