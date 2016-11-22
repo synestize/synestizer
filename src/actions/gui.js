102da9bae9cbe2b89aa878fdb7c8e43522ffbc72
howthebodyworks <git@email.possumpalace.org>
2016-11-22 20:47:17 +1100

    save/load infrastructure

--- src/actions/gui.js
+++ src/actions/gui.js
@@ -1,25 +1,5 @@
  export const SET_VISIBLE_PANE = 'SET_VISIBLE_PANE'
- export const RESET_TO_NOTHING = 'RESET_TO_NOTHING'
- export const RESET_TO_DEFAULT = 'RESET_TO_DEFAULT'
 
 export const setVisiblePane = (tab) => {
   return { type: SET_VISIBLE_PANE, payload: tab }
-}
-export const resetToNothing = () => {
-  return { type: RESET_TO_NOTHING, payload: undefined }
-}
-export const resetToDefault = () => {
-  // Thunk middleware to allow adaptive reset
-  return function(dispatch, getState) {
-    // We need to know what signals are available
-    // to know what the defaults should be
-    let state = getState()
-    return dispatch({
-      type: RESET_TO_DEFAULT,
-      payload: {
-        signal: state.signal,
-        audio: state.audio
-      }
-    })
-  }
 }
