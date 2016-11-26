f625031f1942d99eb4db464616f8713cc1bedfda
howthebodyworks <git@email.possumpalace.org>
2016-11-26 13:22:32 +1100

    record widget - nearly forgot it

---
+++ src/containers/RecordWidget.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import { resetToNothing } from '../actions/gui';
+import Button from '../components/Button';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    text: 'NUKE'
+  }
+};
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      dispatch(resetToNothing())
+      window.location.reload()
+    }
+  }
+};
+
+const RecordWidget = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)( Button );
+
+export default RecordWidget;
