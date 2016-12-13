1a570a3e3434c5fa749e95aaf4a2cd986602ae57
howthebodyworks <git@email.possumpalace.org>
2016-12-13 10:38:06 +0100

    optimize select boxes using reselect

--- src/containers/ComboSignalSelect.js
+++ src/containers/ComboSignalSelect.js
@@ -1,17 +1,32 @@
 import { connect } from 'react-redux';
 // import { setValidAudioSource, setAudioSourceDevice, setAllAudioSources } from '../actions/audio'
 import Select from '../components/Select.js'
+import { createSelector } from 'reselect'
+
+
+const getComboSignalMeta = state => state.signal.comboSignalMeta
+const getComboSignalOptDict = createSelector(
+  [ getComboSignalMeta ],
+  (comboSignalMeta) => {
+    const optDict = {}
+    let keys = Array.from(Object.keys(comboSignalMeta)).sort();
+    for (let key of keys) {
+      optDict[key] = (
+        comboSignalMeta[key].owner
+        + '/'
+        + comboSignalMeta[key].name
+      )
+    }
+    return optDict
+  }
+)
 
 const mapStateToProps = (state, {sinkControlKey, onSignalChange}) => {
   const signalMeta = state.signal.comboSignalMeta
   const optDict = {}
-  let keys = Array.from(Object.keys(signalMeta)).sort();
-  for (let key of keys) {
-    optDict[key] = signalMeta[key].owner + '/' + signalMeta[key].name
-  }
 
   return {
-    optDict,
+    optDict: getComboSignalOptDict(state),
     currentOpt: (state.audio.sinkControls[sinkControlKey] || {}).signal,
     // NB this is a dispatcher - should move it
     onChange: (key) => onSignalChange(key)
