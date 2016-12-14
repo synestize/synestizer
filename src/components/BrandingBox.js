ed1649bd2bfe14eaca6fa52d488e5e29ebb0e5c7
howthebodyworks <git@email.possumpalace.org>
2016-12-14 18:25:43 +0100

    delete everything boring

--- src/components/BrandingBox.js
+++
@@ -1,9 +0,0 @@
-
-const BrandingBox = () => (<div className='branding-box'>
-  <div className='branding-info'>
-    <h1>Synestizer <span className='version'>{EDITION} edition.</span></h1>
-    <span>v{VERSION}</span>
-  </div>
-</div>)
-
-export default BrandingBox;
--- src/io/signal/util.js
+++
@@ -1,6 +0,0 @@
-import { greekLetters } from '../../lib/names'
-import { pad4 } from '../../lib/string'
-
-export function genericSinkSignalName(i) {
-  return ['generic-'+ pad4(i), greekLetters[i]]
-}
